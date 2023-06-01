dabc647e2e5ef025cbbd9ebe97dcb0b68e383d8e
Jody LeCompte <jody@jodylecompte.com>
2023-06-01 16:18:28 -0500

    Fix incorrect object property name

--- Backend Development and APIs Certification/Timestamp Microservice/server.js
+++ Backend Development and APIs Certification/Timestamp Microservice/server.js
@@ -2,27 +2,27 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/:date?", function(req, res) {
+app.get("/api/:date?", function (req, res) {
   const { date } = req.params;
   let asDate;
 
-   if (!date || date === "") {
+  if (!date || date === "") {
     // If no date is provided, use the current date
     asDate = new Date();
   } else if (isNaN(date)) {
@@ -41,11 +41,11 @@ app.get("/api/:date?", function(req, res) {
 
   res.json({
     unix: asDate.getTime(),
-    natural: asDate.toUTCString()
+    utc: asDate.toUTCString(),
   });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
